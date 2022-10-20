f3fb709464f9fe13e5f2767c3ba7bf2bda6fca13
KITHINGOMBO <111295103+KITHINGOMBO@users.noreply.github.com>
2022-10-20 21:36:32 +0300

    Handles conversion specifiers

--- functions.c
+++ functions.c
@@ -33,6 +33,7 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
+	
 	int length = 0, i;
 	char *str = va_arg(types, char *);
 
